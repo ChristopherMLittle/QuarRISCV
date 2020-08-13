4cbf2b50936af8673cc2f182e8e1c71d13e4c240
ChristopherMLittle <chrismlittle12@gmail.com>
2020-08-13 13:18:55 -0600

    Adding ip and flow image

---
+++ RISCV_Test_Verilog.srcs/ip/Memory_Interface_Controller_1.0/src/design_1_wrapper.v
@@ -0,0 +1,224 @@
+//Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2019.2 (win64) Build 2708876 Wed Nov  6 21:40:23 MST 2019
+//Date        : Sat May 30 20:13:08 2020
+//Host        : Chris-GT60 running 64-bit Service Pack 1  (build 7601)
+//Command     : generate_target design_1_wrapper.bd
+//Design      : design_1_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module design_1_wrapper
+   (axi_aclk,
+    axi_aresetn,
+    axi_init_axi_txn,
+    cc_raddr,
+    cc_rdata,
+    cc_wdata,
+    cc_we,
+    m00_cc_re,
+    m_axi_araddr,
+    m_axi_arburst,
+    m_axi_arcache,
+    m_axi_arid,
+    m_axi_arlen,
+    m_axi_arlock,
+    m_axi_arprot,
+    m_axi_arqos,
+    m_axi_arready,
+    m_axi_arsize,
+    m_axi_aruser,
+    m_axi_arvalid,
+    m_axi_awaddr,
+    m_axi_awburst,
+    m_axi_awcache,
+    m_axi_awid,
+    m_axi_awlen,
+    m_axi_awlock,
+    m_axi_awprot,
+    m_axi_awqos,
+    m_axi_awready,
+    m_axi_awsize,
+    m_axi_awuser,
+    m_axi_awvalid,
+    m_axi_bid,
+    m_axi_bready,
+    m_axi_bresp,
+    m_axi_buser,
+    m_axi_bvalid,
+    m_axi_error,
+    m_axi_rdata,
+    m_axi_rid,
+    m_axi_rlast,
+    m_axi_rready,
+    m_axi_rresp,
+    m_axi_ruser,
+    m_axi_rvalid,
+    m_axi_txn_done,
+    m_axi_wdata,
+    m_axi_wlast,
+    m_axi_wready,
+    m_axi_wstrb,
+    m_axi_wuser,
+    m_axi_wvalid);
+  input axi_aclk;
+  input axi_aresetn;
+  input axi_init_axi_txn;
+  input [31:0]cc_raddr;
+  output [127:0]cc_rdata;
+  input [127:0]cc_wdata;
+  input cc_we;
+  input m00_cc_re;
+  output [31:0]m_axi_araddr;
+  output [1:0]m_axi_arburst;
+  output [3:0]m_axi_arcache;
+  output [0:0]m_axi_arid;
+  output [7:0]m_axi_arlen;
+  output m_axi_arlock;
+  output [2:0]m_axi_arprot;
+  output [3:0]m_axi_arqos;
+  input m_axi_arready;
+  output [2:0]m_axi_arsize;
+  output [0:0]m_axi_aruser;
+  output m_axi_arvalid;
+  output [31:0]m_axi_awaddr;
+  output [1:0]m_axi_awburst;
+  output [3:0]m_axi_awcache;
+  output [0:0]m_axi_awid;
+  output [7:0]m_axi_awlen;
+  output m_axi_awlock;
+  output [2:0]m_axi_awprot;
+  output [3:0]m_axi_awqos;
+  input m_axi_awready;
+  output [2:0]m_axi_awsize;
+  output [0:0]m_axi_awuser;
+  output m_axi_awvalid;
+  input [0:0]m_axi_bid;
+  output m_axi_bready;
+  input [1:0]m_axi_bresp;
+  input [0:0]m_axi_buser;
+  input m_axi_bvalid;
+  output m_axi_error;
+  input [31:0]m_axi_rdata;
+  input [0:0]m_axi_rid;
+  input m_axi_rlast;
+  output m_axi_rready;
+  input [1:0]m_axi_rresp;
+  input [0:0]m_axi_ruser;
+  input m_axi_rvalid;
+  output m_axi_txn_done;
+  output [31:0]m_axi_wdata;
+  output m_axi_wlast;
+  input m_axi_wready;
+  output [3:0]m_axi_wstrb;
+  output [0:0]m_axi_wuser;
+  output m_axi_wvalid;
+
+  wire axi_aclk;
+  wire axi_aresetn;
+  wire axi_init_axi_txn;
+  wire [31:0]cc_raddr;
+  wire [127:0]cc_rdata;
+  wire [127:0]cc_wdata;
+  wire cc_we;
+  wire m00_cc_re;
+  wire [31:0]m_axi_araddr;
+  wire [1:0]m_axi_arburst;
+  wire [3:0]m_axi_arcache;
+  wire [0:0]m_axi_arid;
+  wire [7:0]m_axi_arlen;
+  wire m_axi_arlock;
+  wire [2:0]m_axi_arprot;
+  wire [3:0]m_axi_arqos;
+  wire m_axi_arready;
+  wire [2:0]m_axi_arsize;
+  wire [0:0]m_axi_aruser;
+  wire m_axi_arvalid;
+  wire [31:0]m_axi_awaddr;
+  wire [1:0]m_axi_awburst;
+  wire [3:0]m_axi_awcache;
+  wire [0:0]m_axi_awid;
+  wire [7:0]m_axi_awlen;
+  wire m_axi_awlock;
+  wire [2:0]m_axi_awprot;
+  wire [3:0]m_axi_awqos;
+  wire m_axi_awready;
+  wire [2:0]m_axi_awsize;
+  wire [0:0]m_axi_awuser;
+  wire m_axi_awvalid;
+  wire [0:0]m_axi_bid;
+  wire m_axi_bready;
+  wire [1:0]m_axi_bresp;
+  wire [0:0]m_axi_buser;
+  wire m_axi_bvalid;
+  wire m_axi_error;
+  wire [31:0]m_axi_rdata;
+  wire [0:0]m_axi_rid;
+  wire m_axi_rlast;
+  wire m_axi_rready;
+  wire [1:0]m_axi_rresp;
+  wire [0:0]m_axi_ruser;
+  wire m_axi_rvalid;
+  wire m_axi_txn_done;
+  wire [31:0]m_axi_wdata;
+  wire m_axi_wlast;
+  wire m_axi_wready;
+  wire [3:0]m_axi_wstrb;
+  wire [0:0]m_axi_wuser;
+  wire m_axi_wvalid;
+
+  design_1 design_1_i
+       (.axi_aclk(axi_aclk),
+        .axi_aresetn(axi_aresetn),
+        .axi_init_axi_txn(axi_init_axi_txn),
+        .cc_raddr(cc_raddr),
+        .cc_rdata(cc_rdata),
+        .cc_wdata(cc_wdata),
+        .cc_we(cc_we),
+        .m00_cc_re(m00_cc_re),
+        .m_axi_araddr(m_axi_araddr),
+        .m_axi_arburst(m_axi_arburst),
+        .m_axi_arcache(m_axi_arcache),
+        .m_axi_arid(m_axi_arid),
+        .m_axi_arlen(m_axi_arlen),
+        .m_axi_arlock(m_axi_arlock),
+        .m_axi_arprot(m_axi_arprot),
+        .m_axi_arqos(m_axi_arqos),
+        .m_axi_arready(m_axi_arready),
+        .m_axi_arsize(m_axi_arsize),
+        .m_axi_aruser(m_axi_aruser),
+        .m_axi_arvalid(m_axi_arvalid),
+        .m_axi_awaddr(m_axi_awaddr),
+        .m_axi_awburst(m_axi_awburst),
+        .m_axi_awcache(m_axi_awcache),
+        .m_axi_awid(m_axi_awid),
+        .m_axi_awlen(m_axi_awlen),
+        .m_axi_awlock(m_axi_awlock),
+        .m_axi_awprot(m_axi_awprot),
+        .m_axi_awqos(m_axi_awqos),
+        .m_axi_awready(m_axi_awready),
+        .m_axi_awsize(m_axi_awsize),
+        .m_axi_awuser(m_axi_awuser),
+        .m_axi_awvalid(m_axi_awvalid),
+        .m_axi_bid(m_axi_bid),
+        .m_axi_bready(m_axi_bready),
+        .m_axi_bresp(m_axi_bresp),
+        .m_axi_buser(m_axi_buser),
+        .m_axi_bvalid(m_axi_bvalid),
+        .m_axi_error(m_axi_error),
+        .m_axi_rdata(m_axi_rdata),
+        .m_axi_rid(m_axi_rid),
+        .m_axi_rlast(m_axi_rlast),
+        .m_axi_rready(m_axi_rready),
+        .m_axi_rresp(m_axi_rresp),
+        .m_axi_ruser(m_axi_ruser),
+        .m_axi_rvalid(m_axi_rvalid),
+        .m_axi_txn_done(m_axi_txn_done),
+        .m_axi_wdata(m_axi_wdata),
+        .m_axi_wlast(m_axi_wlast),
+        .m_axi_wready(m_axi_wready),
+        .m_axi_wstrb(m_axi_wstrb),
+        .m_axi_wuser(m_axi_wuser),
+        .m_axi_wvalid(m_axi_wvalid));
+endmodule
